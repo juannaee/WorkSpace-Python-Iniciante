c2a25e2a38002eeb538efb8d08b7845668121b02
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-05-30 09:12:17 -0300

    Modificação 30/05/2023 - 09:12

--- Python-Intermediario/funcoes/funcoes4.py
+++ Python-Intermediario/funcoes/funcoes4.py
@@ -1,13 +1,23 @@
+"""Testando Função com a bibliotecca numpy, com valores recebidos pelo input"""
 import numpy as np
+import os
+
+os.system("cls")
 
 
 def multiplication(*args):
+    # np.prod tem a ação de multiplicar todos os argumentos
     return np.prod(args)
 
 
-print(multiplication(2, 2, 3))
+numero_de_interacoes = int(input("Digite quantos valores serão inseridos: "))
+lista_valores = []
 
+"""Criando For para controlar o numero de entrada de valores"""
+for i in range(numero_de_interacoes):
+    valor = int(input("Digite um valor: "))
+    # Adicionando valores na lista
+    lista_valores.append(valor)
 
-lista = [1, 2, 3, 4, 5]
-lista.append(9)
-print(lista)
+resultado = multiplication(*lista_valores)
+print(f"{resultado= }")
