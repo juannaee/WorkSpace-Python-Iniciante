c666f3e08b8aa3d365ee8132db9a90c5d6b12b71
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-07-12 08:33:48 -0300

    Modificação JSON 12/07/2023 08:33

--- Python-Intermediario/Calculadora/main2.py
+++ Python-Intermediario/Calculadora/main2.py
@@ -1,61 +1,67 @@
 import os
 
 resultado = 0
-os.system('cls')
+os.system("cls")
+
 
 def sum(value_1, value_2):
     return float(value_1) + float(value_2)
 
+
 def subtrahend(value_1, value_2):
     if float(value_1) < float(value_2):
         return float(value_2) - float(value_1)
+
     else:
         return float(value_1) - float(value_2)
+
 
 def multiplicacao(value_1, value_2):
     return float(value_1) * float(value_2)
 
+
 def divisao(value_1, value_2):
     return float(value_2) / float(value_1)
 
+
 while True:
-    print('Escolha uma opção do menu:')
-    print('1 - Entrar na calculadora\n2 - Sair')
-    print('')
-    opcao_menu = input('Digite uma das opções:')
-    print('')
+    print("Escolha uma opção do menu:")
+    print("1 - Entrar na calculadora\n2 - Sair")
+    print("")
+    opcao_menu = input("Digite uma das opções:")
+    print("")
     try:
         opcao_menu = int(opcao_menu)
     except ValueError:
-        print('Opção inválida! Você deve digitar um numero para escolher uma opção!!')
+        print("Opção inválida! Você deve digitar um numero para escolher uma opção!!")
         continue
 
     if opcao_menu == 1:
         while True:
-            print('')
-            print('Entrar calculadora.....')
-            print('')
-            print('Escolha uma das opções: ')
-            print(
-                '1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair')
-            opcao_calculadora = input('Digite uma das opção:')
-            print('')
+            print("")
+            print("Entrar calculadora.....")
+            print("")
+            print("Escolha uma das opções: ")
+            print("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair")
+            opcao_calculadora = input("Digite uma das opção:")
+            print("")
 
             try:
                 opcao_calculadora = int(opcao_calculadora)
             except ValueError:
                 print(
-                    'Opção inválida! Você deve digitar um numero para escolher uma opção!!')
+                    "Opção inválida! Você deve digitar um numero para escolher uma opção!!"
+                )
                 continue
 
-            valor_1 = input('Entrar com o primeiro valor:')
-            valor_2 = input('Entrar com o segundo valor:')
+            valor_1 = input("Entrar com o primeiro valor:")
+            valor_2 = input("Entrar com o segundo valor:")
 
             try:
                 valor_1 = float(valor_1)
                 valor_2 = float(valor_2)
             except ValueError:
-                print('Você deve digitar uma numero para realizar a operação!')
+                print("Você deve digitar uma numero para realizar a operação!")
                 continue
 
             if opcao_calculadora == 1:
@@ -75,14 +81,14 @@ while True:
                 print(resultado)
 
             elif opcao_calculadora == 5:
-                print('Certo! Saindo......')
+                print("Certo! Saindo......")
                 break
 
             else:
-                print('Opção inválida, escolha uma das opções!!!!!!!')
+                print("Opção inválida, escolha uma das opções!!!!!!!")
     elif opcao_menu == 2:
-        print('Saindo....')
+        print("Saindo....")
         break
     else:
-        print('Opção inválida, escolha uma das opções!!!!!!!')
+        print("Opção inválida, escolha uma das opções!!!!!!!")
         continue
