a00157498374fd7317795cf25da0767f909c168f
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-07-19 15:11:39 -0300

    Modificação 19/07/2023 15:11 TRABALHO

---
+++ Python-Intermediario/List comprehension/list3.py
@@ -0,0 +1,29 @@
+import pprint
+
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False)
+
+
+lista_usuarios = [
+    {"Nome": "JUAN", "STATUS": "Ativo", "teste": 1},
+    {"Nome": "BRENNO", "STATUS": "Ativo", "teste": 0},
+    {"Nome": "SAMUEL", "STATUS": "Ativo", "teste": 0},
+    {"Nome": "JAIRO", "STATUS": "Ativo", "teste": 1},
+    {"Nome": "Roberta", "STATUS": "Inativo", "teste": 1},
+    {"Nome": "Bella", "STATUS": "Inativo", "teste": 1},
+]
+
+lista_usuarios_inativo = [
+    {"Nome": "JUAN", "STATUS": "Inativo"},
+    {"Nome": "BRENNO", "STATUS": "Inativo"},
+]
+
+lista_usuarios_att = [
+    {**usuario, "STATUS": usuario["STATUS"] == "Inativo"}
+    if usuario["teste"] == 0
+    else {**usuario}
+    for usuario in lista_usuarios
+]
+
+p(lista_usuarios_att)
