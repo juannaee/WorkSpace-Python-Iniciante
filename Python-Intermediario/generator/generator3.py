10a40a09070c4f44638fa1d37560217e2fd785d5
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-07-24 18:07:37 -0300

    Modificação 24/07/2023 18:06 TRABALHO

--- Python-Intermediario/generator/generator3.py
+++ Python-Intermediario/generator/generator3.py
@@ -17,6 +17,24 @@ def subtracao(v1, v2):
         print("Valores nao numericos!")
 
 
+def divisao(v1, v2):
+    try:
+        v1 = int(v1)
+        v2 = int(v2)
+        return True, v1 / v2
+    except ValueError:
+        print("Valores nao numericos!")
+
+
+def multiplicacao(v1, v2):
+    try:
+        v1 = int(v1)
+        v2 = int(v2)
+        return True, v1 * v2
+    except ValueError:
+        print("Valores nao numericos!")
+
+
 def flag_generator(v=0):
     yield f"RODANDO EXECUTAVEL #{v}: SOMA"
     resultado = soma(4, 5)
@@ -37,7 +55,32 @@ def flag_generator(v=0):
     return "FINALIZADO"
 
 
-gen = flag_generator(1)
+def flag_generator_2(v, gen):
+    yield from gen
+    yield f"RODANDO EXECUTAVEL #{v+2}: MULTIPLICACAO"
+    resultado = multiplicacao(2, 2)
+    if resultado is not None:
+        print(f"resultado:{resultado[1]}")
+        print("!!PASSOU!!")
+    else:
+        print("ERROR!")
 
-for i in gen:
+    yield f"RODANDO EXECUTAVEL #{v+3}: DIVISAO"
+    resultado = divisao(10, 5)
+    if resultado is not None:
+        print(f"resultado:{resultado[1]}")
+        print("!!PASSOU!!")
+    else:
+        print("ERROR!")
+
+    return "FINALIZADO"
+
+
+gen = flag_generator(1)
+gen_2 = flag_generator_2(1, flag_generator(1))
+
+# for i in gen:
+#     print(i)
+
+for i in gen_2:
     print(i)
