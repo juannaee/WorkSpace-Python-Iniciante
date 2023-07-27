b1b1fc0c2c81c6914caf87d8578ac2f0fa96e49d
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-07-27 11:43:59 -0300

    Modificação 27/07/2023 11:43 Trabalho

--- Python-Intermediario/generator/generator3.1.py
+++ Python-Intermediario/generator/generator3.1.py
@@ -9,9 +9,10 @@ def soma(*args):
             total = sum(args)
             return True, total
         else:
-            raise TypeError
-    except TypeError:
-        print("VALORES NAO NUMERICOS")
+            raise TypeError("Existem valores nao numericos")
+    except TypeError as error:
+        print(f"ERROR: {error}")
+        print(f"Tipo excecao: {error.__class__.__name__}")
         return None
 
 
@@ -25,30 +26,26 @@ def mult(*args):
                 total = total * i
             return True, total
         else:
-            raise TypeError
-    except TypeError:
-        print("VALORES NAO NUMERICOS")
+            raise TypeError("Existem valores nao numericos")
+    except TypeError as error:
+        print(f"{error}")
+        print(f"Tipo excecao: {error.__class__.__name__}")
         return None
 
 
 def func_generator_():
     v = 0
     yield f"EXECUTANDO SCRIPT #{v + 1} FUNCAO: '{soma.__name__}'"
-    resultado = soma(5, 5, 5, 5, 5, 5, 5, "S")
+    resultado = soma("a", 2)
 
     if resultado is not None:
         print(f"Resultado:{resultado[1]}")
-    else:
-        print(f"ERRO SCRIPT '{soma.__name__}'")
-
     print("<====================================================>")
     yield f"EXECUTANDO SCRIPT #{v + 2} FUNCAO: '{mult.__name__}'"
     resultado = mult(2, 10)
 
     if resultado is not None:
         print(f"RESULTADO: {resultado[1]}")
-    else:
-        print(f"ERRO SCRIPT '{soma.__name__}'")
 
 
 gen = func_generator_()
