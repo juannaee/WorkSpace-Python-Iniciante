3b1f650e11ecfdc3d14712451e9e3db2e32eb349
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-07-27 10:38:41 -0300

    Modificação 27/07/2023 10:38 Trabalho

--- Python-Intermediario/generator/generator3.1.py
+++ Python-Intermediario/generator/generator3.1.py
@@ -15,17 +15,42 @@ def soma(*args):
         return None
 
 
-def flag_generator_():
+def mult(*args):
+    total = 0
+    try:
+        if total == 0:
+            total = +1
+        if sao_todos_numeros(*args):
+            for i in args:
+                total = total * i
+            return True, total
+        else:
+            raise TypeError
+    except TypeError:
+        print("VALORES NAO NUMERICOS")
+        return None
+
+
+def func_generator_():
     v = 0
-    yield f"EXECUTANDO SCRIPT #{v + 1} FUNCAO: {soma.__name__}"
+    yield f"EXECUTANDO SCRIPT #{v + 1} FUNCAO: '{soma.__name__}'"
     resultado = soma(5, 5, 5, 5, 5, 5, 5, "S")
 
     if resultado is not None:
         print(f"Resultado:{resultado[1]}")
     else:
-        print(f"ERRO SCRIPT {soma.__name__}")
+        print(f"ERRO SCRIPT '{soma.__name__}'")
+
+    print("<====================================================>")
+    yield f"EXECUTANDO SCRIPT #{v + 2} FUNCAO: '{mult.__name__}'"
+    resultado = mult(2, 10)
+
+    if resultado is not None:
+        print(f"RESULTADO: {resultado[1]}")
+    else:
+        print(f"ERRO SCRIPT '{soma.__name__}'")
 
 
-gen = flag_generator_()
+gen = func_generator_()
 for i in gen:
     print(i)
