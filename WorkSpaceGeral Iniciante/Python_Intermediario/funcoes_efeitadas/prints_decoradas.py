6213a27a599774bbea9bd8e9659ebc3443c2c3e8
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-08-22 18:34:53 -0300

    Modifocação 22/08/2023 18:34

--- WorkSpaceGeral Iniciante/Python_Intermediario/funcoes_efeitadas/prints_decoradas.py
+++
@@ -1,9 +0,0 @@
-def print_decorated_header(title):
-    title_length = len(title)
-    top_border = " ╔" + "═" * (title_length + 4) + "╗ "
-    bottom_border = " ╚" + "═" * (title_length + 4) + "╝ "
-    content = f" ║  {title}  ║ "
-
-    print(top_border)
-    print(content)
-    print(bottom_border)
